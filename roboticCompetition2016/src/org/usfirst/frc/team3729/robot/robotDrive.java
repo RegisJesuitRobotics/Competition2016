21d4bfd3a4ead8d1767062f7d559f1e94a93e464
noahlordi <n.p.lordi@gmail.com>
2016-01-26 16:15:34 -0700

    new class

---
+++ roboticCompetition2016/src/org/usfirst/frc/team3729/robot/robotDrive.java
@@ -0,0 +1,93 @@
+package org.usfirst.frc.team3729.robot;
+
+import edu.wpi.first.wpilibj.CANTalon;
+
+public class robotDrive {
+	CANTalon RightMotor1, LeftMotor1, RightMotor2, LeftMotor2;
+	XboxController xbox;
+	public robotDrive()
+	{
+		RightMotor1 = new CANTalon(2);
+		RightMotor2 = new CANTalon(3);
+		LeftMotor1 = new CANTalon(1);
+		LeftMotor2 = new CANTalon(4);
+		xbox = new XboxController(0);
+	}
+
+	public void arcadeDrive() {
+		
+
+		// This limits the power of the motor, it is a percentage
+		// This SHOULD NOT go above 1.0, not should it be negative
+		double motorLimiterRatioinital = .5;
+		double motorLimiterRatio = motorLimiterRatioinital;
+		double forwardInput = xbox.GetForwardInput();
+		double turnInput = xbox.GetTurnInput();
+		double leftMotorInput = 0;
+		double rightMotorInput = 0;
+		double deadZone = 0.2;
+
+		// System.out.println(forwardInput);
+		// System.out.println(turnInput + "turn");
+
+		if (forwardInput > deadZone && turnInput > deadZone) {
+			leftMotorInput = forwardInput;
+			rightMotorInput = turnInput * .25;
+			System.out.println("turn forward right");
+			// Turn Forward Right
+		} else if (forwardInput <= deadZone && forwardInput >= -deadZone
+				&& turnInput > deadZone) {
+			leftMotorInput = turnInput;
+			rightMotorInput = -turnInput;
+			System.out.println("spin right");
+			// Spin Right
+		} else if (forwardInput > deadZone && turnInput <= deadZone
+				&& turnInput >= -deadZone) {
+			leftMotorInput = forwardInput;
+			rightMotorInput = forwardInput;
+			System.out.println("Forward");
+			// Move Forward
+		} else if (forwardInput > deadZone && turnInput < deadZone) {
+			// Left input is negative, so it must be negated to move forward.
+			leftMotorInput = -turnInput * .25;
+			rightMotorInput = forwardInput;
+			System.out.println("turn forward left");
+			// Turn Forwards Left
+		} else if (forwardInput <= deadZone && forwardInput >= -deadZone
+				&& turnInput < -deadZone) {
+			// Left motor should move in reverse, right should move forward.
+			// Left turn is a negative input already, so we don't need to negate
+			// it again.
+			leftMotorInput = turnInput;
+			rightMotorInput = -turnInput;
+			System.out.println("spin left");
+			// Spin Left
+		} else if (forwardInput < -deadZone && turnInput < -deadZone) {
+			leftMotorInput = turnInput * .25;
+			rightMotorInput = forwardInput;
+			System.out.println("turn backwards left");
+			// Turn Backwards Left
+		} else if (forwardInput < -deadZone && turnInput > deadZone) {
+			leftMotorInput = forwardInput;
+			rightMotorInput = -turnInput * .25;
+			System.out.println("turn backwards right");
+			// Turn Backwards Right
+		} else if (forwardInput < -deadZone) {
+			leftMotorInput = forwardInput;
+			rightMotorInput = forwardInput;
+			System.out.println("move backwards");
+			// Move Backwards
+		}
+		if (xbox.GetRightTrigger() > deadZone) {
+			motorLimiterRatio += (xbox.GetRightTrigger() * .5);
+		} else {
+			motorLimiterRatio = motorLimiterRatioinital;
+		}
+		RightMotor1.set(-rightMotorInput * motorLimiterRatio);
+		LeftMotor1.set(leftMotorInput * motorLimiterRatio);
+		RightMotor2.set(-rightMotorInput * motorLimiterRatio);
+		LeftMotor2.set(leftMotorInput * motorLimiterRatio);
+		// System.out.println(leftMotorInput + "left");
+		// System.out.println(rightMotorInput + "right");
+	}
+}
