178ff918f741d6192bffb8b063d0520ffb2feb51
Mr. H <mheidenry@regisjesuit.com>
2016-02-07 11:22:44 -0700

    This is Garrison logged in as Coach H on the Fujitsu.  Adding gyro and ultrasonic code (some of it is commented).

--- roboticCompetition2016/twoWheelDriveENCODER/src/org/usfirst/frc/team3729/robot/Robot.java
+++ roboticCompetition2016/twoWheelDriveENCODER/src/org/usfirst/frc/team3729/robot/Robot.java
@@ -1,102 +1,181 @@
 
 package org.usfirst.frc.team3729.robot;
 
-
-import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.SampleRobot;
-import edu.wpi.first.wpilibj.Servo;
+import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.Ultrasonic;
 
 /**
- * This is a demo program showing the use of the RobotDrive class.
- * The SampleRobot class is the base of a robot application that will automatically call your
- * Autonomous and OperatorControl methods at the right time as controlled by the switches on
- * the driver station or the field controls.
- *
+ * 
  * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the SampleRobot
+ * 
+ * functions corresponding to each mode, as described in the IterativeRobot
+ * 
  * documentation. If you change the name of this class or the package after
+ * 
  * creating this project, you must also update the manifest file in the resource
+ * 
  * directory.
- *
- * WARNING: While it may look like a good choice to use for your code if you're inexperienced,
- * don't. Unless you know what you are doing, complex code will be much more difficult under
- * this system. Use IterativeRobot or Command-Based instead if you're new.
+ * 
  */
-public class Robot extends SampleRobot {
-    
-	
-	Input input;
-    Encoder leftENC;
-    Encoder rightENC;
-    int encoderCountLEFT;
-    Talon mcLeft;
-    Talon mcRight;
-    robotDrive drive;
-    Servo camServo;
-    stringPotentiometer pot;
-    
-    public Robot() {
-        mcLeft = new Talon(0);
-        mcRight = new Talon(1);
-        drive = new robotDrive(mcLeft, mcRight);
-        pot = new stringPotentiometer();
-        //drive.setExpiration(0.1);
-        input = new Input();
-        leftENC = new Encoder(0,1,false,Encoder.EncodingType.k2X);
-        leftENC.setMaxPeriod(1);//maximum time in which the device is still considered moving
-        leftENC.setMinRate(10);//min rate b/4 considered stopped
-        leftENC.setDistancePerPulse(5);//sets scale factor between pulse and distance
-        leftENC.setReverseDirection(true);//depending on mounting of motor
-        leftENC.setSamplesToAverage(7);//averaging reduces jittery movement
-        camServo = new Servo(2);
-        rightENC= new Encoder(2,3,false,Encoder.EncodingType.k2X);
-        
-        //parameters: (ports)a channel, b channel, reverse direction, encoding type
-    }
 
-    /**
-     * Drive left & right motors for 2 seconds then stop
-     */
-    @SuppressWarnings("deprecation")
-	public void autonomous() {
-    	
-    	leftENC.reset();//needed b/c encoder starts counting as soon as it is created
-    	encoderCountLEFT = leftENC.get();
-    	System.out.println("Encoder distance "+leftENC.getDistance());
-    	System.out.println("Encoder period "+leftENC.getPeriod());
-    	System.out.println("Encoder rate "+leftENC.getRate());
-    	
-    	
-    	
-    	
-        drive.setSafetyEnabled(false);
-        drive.tankDrive(-0.5, 0.5);	// drive forwards half speed
-        Timer.delay(2.0);		//    for 2 seconds
-        drive.tankDrive(0.0, 0.0);	// stop robot
-    }
+public class Robot extends IterativeRobot {
 
-    /**
-     * Runs the motors with arcade steering.
-     */
-    public void operatorControl() {
-       drive.setSafetyEnabled(true);
-        while (isOperatorControl() && isEnabled()) {
-        	drive.tankDrive();
-        	Timer.delay(0.005);		// wait for a motor update time
-        
-        	System.out.println(pot.ai.getVoltage());
-        	
-            
-            camServo.set (input.left.getPOV());
-            //System.out.println(stickL.getPOV());
-        }
-    }
+	/**
+	 * 
+	 * This function is run when the robot is first started up and should be
+	 * 
+	 * used for any initialization code.
+	 * 
+	 */
+	AnalogGyro gyro; // must be on analog 1 or 0
+	Talon leftMotor = new Talon(0);
+	Talon rightMotor = new Talon(1);
+	Ultrasonic front = new Ultrasonic(0, 1);
+	Ultrasonic right = new Ultrasonic(6, 7);
+	Ultrasonic left = new Ultrasonic(4, 5);
+	boolean isAutoProceduralRunning = false;
 
-    /**
-     * Runs during test mode
-     */
-    public void test() {
-    }
+	// creates the ultra object andassigns ultra to be an ultrasonic sensor
+	// which uses DigitalOutput 1 for
+
+	// the echo pulse and DigitalInput 1 for the trigger pulse
+
+	public void robotInit() {
+
+		front.setAutomaticMode(true);
+		right.setAutomaticMode(true);
+		left.setAutomaticMode(true);// turns on automatic mode
+		gyro = new AnalogGyro(0);
+		gyro.initGyro();
+		gyro.calibrate();
+
+	}
+
+	public void autonomousInit() {
+		gyro.reset();
+	}
+
+	/**
+	 * 
+	 * This function is called periodically during autonomous
+	 * 
+	 */
+
+	public void autonomousPeriodic() {
+
+		// double rangeFront = front.getRangeInches(); // reads the range on the
+		// // ultrasonic sensor
+		// double rangeLeft = left.getRangeInches();
+		// double rangeRight = right.getRangeInches();
+		//
+		// if ((rangeFront > 20) && (rangeLeft > 20) && (rangeRight > 20)) {
+		// leftMotor.set(-0.4);
+		// rightMotor.set(0.4);
+		// }
+		//
+		// else if ((rangeFront > 20) && (rangeLeft > 20) && (rangeRight < 20))
+		// {
+		// leftMotor.set(0.3);
+		// rightMotor.set(0.3);
+		// } else if ((rangeFront > 20) && (rangeLeft < 20) && (rangeRight >
+		// 20)) {
+		// leftMotor.set(-0.3);
+		// rightMotor.set(-0.3);
+		// } else if ((rangeFront < 20) && (rangeLeft > 20) && (rangeRight >
+		// 20)) {
+		// leftMotor.set(0.2);
+		// rightMotor.set(-0.2);
+		// }
+		//
+		// else {
+		// leftMotor.set(0.0);
+		// rightMotor.set(-0.0);
+		//
+		// }
+		//
+		// System.out.println("Front: " + rangeFront + " Left: " + rangeLeft + "
+		// Right: " + rangeRight);
+
+		// Gyro Keep Centerline Test Code
+		// double angle = gyro.getAngle();
+		//
+		// if (angle < -0.03) {
+		// // Turn right to return to center
+		// leftMotor.set(.3);
+		// rightMotor.set(-.4);
+		// } else if (angle > 0.03) {
+		// // Turn left to return to center
+		// leftMotor.set(.6);
+		// rightMotor.set(-.3);
+		// } else {
+		// // Do nothing, robot is centered enough.
+		// }
+
+		// Gyro 90 degree test code
+		if (!isAutoProceduralRunning) {
+			// Effectively stops autonomous from running in a periodic loop.
+			isAutoProceduralRunning = true;
+
+			while (true) {
+				// Move forward for 3 seconds.
+				leftMotor.set(.3);
+				rightMotor.set(-.3);
+				Timer.delay(3);
+
+				// Turn 90
+				double angle = gyro.getAngle();
+				double newAngle = angle - 90;
+
+				System.out.println("Angle" + angle);
+				System.out.println("NewAngle" + newAngle);
+
+				// Want to turn until angle reads angle+90
+				while (gyro.getAngle() > newAngle) {
+					// Run the motors in a turn until we reach out new angle.
+					leftMotor.set(.3);
+					rightMotor.set(.7);
+				}
+			}
+		}
+	}
+
+	public void teleopInit() {
+		System.out.println("test teleop gyro reset");
+
+		gyro.reset();
+		Timer.delay(2);
+	}
+
+	/**
+	 * 
+	 * This function is called periodically during operator control
+	 * 
+	 */
+
+	public void teleopPeriodic() {
+		System.out.println("test");
+		System.out.println("Center: " + gyro.getCenter());
+		System.out.println("Angle: " + gyro.getAngle());
+		System.out.println("Rotation: " + gyro.getRate());
+		try {
+			Thread.sleep(200);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * 
+	 * This function is called periodically during test mode
+	 * 
+	 */
+
+	public void testPeriodic() {
+
+	}
+
 }
